e7dc731a504a7f98d2a0a8b7f6b3396c26848c0d
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-07-07 17:59:59 +0300

    Kate

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Катя")
+
+{
+    Console.WriteLine("Ура, это же Катя!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
